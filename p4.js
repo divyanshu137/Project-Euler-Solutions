f3cc47eb84391ed9040863a2c56934d795c2f449
Divyanshu Malhotra <divyanshu137@gmail.com>
2018-04-04 09:04:14 +0530

    Solution to problem 4

---
+++ p4.js
@@ -0,0 +1,14 @@
+function largestPalindrome() {
+  let largest = 0;
+  for(let i = 999; i > 99; i--) {
+    for(let k = 999; k > 99; k--) {
+      let n = i * k;
+      let s = "" + n;
+      if (s === s.split("").reverse().join("") && n > largest) {
+        largest = n;
+      }
+    }
+  }
+  console.log(largest);
+}
+largestPalindrome();
